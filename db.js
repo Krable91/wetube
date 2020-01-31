58905444dba6b77871fc77262d58e96209dc3924
krable91 <dydansp@naver.com>
2020-01-31 23:19:25 +0900

    wetube video db

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 91,
+        title: "Videos1",
+        description: "This is something I love1",
+        Views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "lee juneho",
+            email: "lee@naver.com"
+        }
+    },
+    {
+        id: 92,
+        title: "Videos2",
+        description: "This is something I love2",
+        Views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "lee juneho",
+            email: "lee@naver.com"
+        }
+    },
+    {
+        id: 93,
+        title: "Videos3",
+        description: "This is something I love3",
+        Views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "lee juneho",
+            email: "lee@naver.com"
+        }
+    },
+    {
+        id: 94,
+        title: "Videos4",
+        description: "This is something I love4",
+        Views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "lee juneho",
+            email: "lee@naver.com"
+        }
+    }
+]
