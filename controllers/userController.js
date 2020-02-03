a3cd5b75bcf18a1fb63e92adc96697106553f08b
krable91 <dydansp@naver.com>
2020-02-04 01:06:34 +0900

    wetube Video Upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,8 +23,11 @@ export const postLogin = (req, res) => {
     res.redirect(routes.home); 
 }
 
-export const logout = (req, res) => 
-res.render("logout", { pageTitle: "Logout"});
+export const logout = (req, res) => {
+    //ToDo: Process Log Out
+    res.redirect(routes.home);
+}
+
 export const users = (req, res) => 
 res.render("users", { pageTitle: "Users"});
 export const userDetail = (req, res) => 
