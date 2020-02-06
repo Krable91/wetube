66134971bbc5075a0a51b5b14fc4414f37db8c5f
krable91 <dydansp@naver.com>
2020-02-07 00:11:21 +0900

    wetube db with mongodb

--- db.js
+++ db.js
@@ -1,4 +1,21 @@
-export const videos = [
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube",
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false
+    }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = () => console.log(`Error on DB Connection:${error}`)
+
+db.once("open", handleOpen);
+db.on("error", handleError);
+
+/*export const videos = [
     {
         id: 91,
         title: "Videos1",
@@ -10,41 +27,5 @@ export const videos = [
             name: "lee juneho",
             email: "lee@naver.com"
         }
-    },
-    {
-        id: 92,
-        title: "Videos2",
-        description: "This is something I love2",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "lee juneho",
-            email: "lee@naver.com"
-        }
-    },
-    {
-        id: 93,
-        title: "Videos3",
-        description: "This is something I love3",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "lee juneho",
-            email: "lee@naver.com"
-        }
-    },
-    {
-        id: 94,
-        title: "Videos4",
-        description: "This is something I love4",
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "lee juneho",
-            email: "lee@naver.com"
-        }
     }
-]
+]*/
