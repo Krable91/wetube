05b26bd14435e186cc80a18217f613378509092d
krable91 <dydansp@naver.com>
2020-02-11 23:42:49 +0900

    wetube async and await, try and catch

--- init.js
+++ init.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 
 
 const handleListening = () => 
-console.log(`Listening on: http://localhost:${PORT}`);
+    console.log(`Listening on: http://localhost:${PORT}`);
 
 
 app.listen(PORT, handleListening);
