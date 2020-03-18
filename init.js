68503ab340856491e7793429c5b7874fd4da2710
krable91 <dydansp@naver.com>
2020-03-19 00:01:16 +0900

    wetube Passport part one

--- init.js
+++ init.js
@@ -1,10 +1,12 @@
-import "./db";
 import dotenv from "dotenv";
+import "./db";
 import app from "./app";
-import "./models/Video";
-import "./models/Comment";
 
 dotenv.config();
+
+import "./models/Video";
+import "./models/Comment";
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
